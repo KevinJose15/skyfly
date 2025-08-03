0256095364922cd996a2bc2fc86a181185124f4b
KevinJose15 <mt25001@esfe.agape.edu.sv>
2025-08-03 01:50:45 -0600

    Se creo la entidad paquete

---
+++ skyfly/src/main/java/esfe/skyfly/Modelos/Paquete.java
@@ -0,0 +1,128 @@
+package esfe.skyfly.Modelos;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import java.time.LocalDate;
+
+@Entity
+public class Paquete {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer paqueteId;
+
+    @NotBlank(message = "El nombre es requerido")
+    private String nombre;
+
+    @NotBlank(message = "La descripción es requerida")
+    private String descripcion;
+
+    @Positive(message = "El precio debe ser positivo")
+    private double precio;
+
+    @Positive(message = "La duración debe ser positiva")
+    private int duracionDias;
+
+    private LocalDate fechaInicio;
+
+    private LocalDate fechaFin;
+
+    private int destinoId;
+
+    @Transient
+    private Destino destino;
+
+    public Paquete() {
+    }
+
+    public Paquete(Integer paqueteId, @NotBlank(message = "El nombre es requerido") String nombre,
+            @NotBlank(message = "La descripción es requerida") String descripcion,
+            @Positive(message = "El precio debe ser positivo") double precio,
+            @Positive(message = "La duración debe ser positiva") int duracionDias, LocalDate fechaInicio,
+            LocalDate fechaFin, int destinoId, Destino destino) {
+        this.paqueteId = paqueteId;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.duracionDias = duracionDias;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.destinoId = destinoId;
+        this.destino = destino;
+    }
+
+    public Integer getPaqueteId() {
+        return paqueteId;
+    }
+
+    public void setPaqueteId(Integer paqueteId) {
+        this.paqueteId = paqueteId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getDuracionDias() {
+        return duracionDias;
+    }
+
+    public void setDuracionDias(int duracionDias) {
+        this.duracionDias = duracionDias;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public int getDestinoId() {
+        return destinoId;
+    }
+
+    public void setDestinoId(int destinoId) {
+        this.destinoId = destinoId;
+    }
+
+    public Destino getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Destino destino) {
+        this.destino = destino;
+    }
+
+    
+}
