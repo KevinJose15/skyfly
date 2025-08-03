6015b2e8ebf4537b472ac365fbf3dd0c25c2dd3f
Henry <jm25002@esfe.agape.edu.sv>
2025-08-03 01:37:30 -0600

    Se creo el modelo Destino

---
+++ skyfly/src/main/java/esfe/skyfly/Modelos/Destino.java
@@ -0,0 +1,70 @@
+package esfe.skyfly.Modelos;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+@Entity
+@Table(name = "Destinos")
+public class Destino {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer destinoId;
+
+    @NotBlank(message = "El nombre es requerido")
+    @Size(max = 100, message = "El nombre no puede tener más de 100 caracteres")
+    private String nombre;
+
+    @NotBlank(message = "El país es requerido")
+    @Size(max = 50, message = "El país no puede tener más de 50 caracteres")
+    private String pais;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @Lob
+    @Column(name = "imagen", columnDefinition = "LONGBLOB")
+    private byte[] imagen;
+
+    public Integer getDestinoId() {
+        return destinoId;
+    }
+
+    public void setDestinoId(Integer destinoId) {
+        this.destinoId = destinoId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public byte[] getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(byte[] imagen) {
+        this.imagen = imagen;
+    }
+
+}
