3aa0af7fbc0339bbed522ec2a306477633ae52ca
Anderson Javier Galicia Ramírez <gr25004@esfe.agape.edu.sv>
2025-08-29 17:46:42 -0600

    Se agregó un método para buscar paquetes por ID de destino en el repositorio de paquetes.

--- skyfly/src/main/java/esfe/skyfly/Repositorios/IPaqueteRepository.java
+++ skyfly/src/main/java/esfe/skyfly/Repositorios/IPaqueteRepository.java
@@ -3,8 +3,9 @@ package esfe.skyfly.Repositorios;
 import esfe.skyfly.Modelos.Paquete;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface IPaqueteRepository extends JpaRepository<Paquete, Integer> {
-    
+      List<Paquete> findByDestino_DestinoId(Integer destinoId);
 }
